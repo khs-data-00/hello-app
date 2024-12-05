ac7799b04e510df6a59f7676593feccf9dde15b2
khs-data-00 <hansix93@naver.com>
2024-12-05 09:19:13 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import plotly.express as px
 
 chart_data=pd.DataFrame(np.random.randn(20,3),columns=['a','b','c'])
 
